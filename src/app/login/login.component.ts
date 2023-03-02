7f9bfc9f5cf8c7ad50161620bd9a7aec02d8042d
trbogi <trencsenyi.bogi@gmail.com>
2023-03-02 11:48:26 +0100

    Create reactive form to submit login data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    "username": new FormControl("", Validators.required),
+    "password": new FormControl("", Validators.required)
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.loginForm.value);
   }
 
 }
