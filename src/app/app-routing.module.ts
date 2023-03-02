d51f322edbaed1ac6a682f9b163d345bb5bc9f63
trbogi <trencsenyi.bogi@gmail.com>
2023-03-03 00:42:04 +0100

    Create AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { CharactersComponent } from './characters/characters.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'characters', component: CharactersComponent,  canActivate: [AuthGuard]}
 ];
 
 @NgModule({
