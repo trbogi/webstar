819afc5cf7192d47d1bce0ad7debf96616b10260
trbogi <trencsenyi.bogi@gmail.com>
2023-03-05 22:01:59 +0100

    Change background dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webstar';
+  backgroundImagePath!: string;
+
+  onRouterOutletActivate($event: any) {
+    console.log($event.backgroundImagePath)
+    this.backgroundImagePath = $event.backgroundImagePath;
+  }
 }
+
